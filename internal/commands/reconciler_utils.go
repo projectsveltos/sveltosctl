d49a022b1962f9b9d52da45f867bc8e34680168a
mgianluc <mgianluca@cisco.com>
2023-10-12 11:32:41 +0200

    Pick long running job fix
    issue described [here](https://github.com/projectsveltos/libsveltos/commit/33de36373269675ab7db84645fb466b6afc17acf)

--- internal/commands/reconciler_utils.go
+++ internal/commands/reconciler_utils.go
@@ -25,7 +25,7 @@ import (
 	"time"
 
 	"github.com/go-logr/logr"
-	"github.com/robfig/cron"
+	"github.com/robfig/cron/v3"
 	corev1 "k8s.io/api/core/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
