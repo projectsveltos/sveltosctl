2bef532fd1120359021609159840efa3aadf44c7
Adam Sadek <33073174+AdamSadek@users.noreply.github.com>
2024-05-23 16:51:22 +0100

    Testing new approach for debuggingconfiguratgions

--- internal/utils/debuggingconfigurations_test.go
+++ internal/utils/debuggingconfigurations_test.go
@@ -57,6 +57,8 @@ var _ = Describe("DebuggingConfigurations", func() {
 		dc := &libsveltosv1alpha1.DebuggingConfiguration{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: utils.DefaultInstanceName,
+				Namespace: namespace,
+				Type: clusterType,
 			},
 		}
 
@@ -65,7 +67,7 @@ var _ = Describe("DebuggingConfigurations", func() {
 		c := fake.NewClientBuilder().WithScheme(scheme).Build()
 
 		k8sAccess := utils.GetK8sAccess(scheme, c)
-		Expect(k8sAccess.UpdateDebuggingConfiguration(context.TODO(), dc, dc.Name, "", "")).To(Succeed())
+		Expect(k8sAccess.UpdateDebuggingConfiguration(context.TODO(), dc, dc.Name, dc.Namespace, dc.Type)).To(Succeed())
 
 		currentDC := &libsveltosv1alpha1.DebuggingConfiguration{}
 		Expect(c.Get(context.TODO(), types.NamespacedName{Name: utils.DefaultInstanceName}, currentDC)).To(Succeed())
@@ -73,7 +75,7 @@ var _ = Describe("DebuggingConfigurations", func() {
 			{Component: libsveltosv1alpha1.ComponentClassifier, LogLevel: libsveltosv1alpha1.LogLevelDebug},
 		}
 
-		Expect(k8sAccess.UpdateDebuggingConfiguration(context.TODO(), currentDC, dc.Name, "", "")).To(Succeed())
+		Expect(k8sAccess.UpdateDebuggingConfiguration(context.TODO(), currentDC, dc.Name, dc.Namespace, dc.Type)).To(Succeed())
 		Expect(c.Get(context.TODO(), types.NamespacedName{Namespace: utils.DefaultInstanceName}, currentDC)).To(Succeed())
 		Expect(len(currentDC.Spec.Configuration)).To(Equal(1))
 	})
